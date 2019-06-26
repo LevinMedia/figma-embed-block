e8ff4bb21a048b92db01f63353b2ebd7c591619c
David Levin <sayhi@levinmedia.com>
2019-06-25 21:16:24 -0700

    time for scotch

--- src/init.php
+++ src/init.php
@@ -27,10 +27,10 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @uses {wp-editor} for WP editor styles.
  * @since 1.0.0
  */
-function gutenberg_figma_embed_cgb_block_assets() { // phpcs:ignore
+function figma_embed_block_cgb_block_assets() { // phpcs:ignore
 	// Register block styles for both frontend + backend.
 	wp_register_style(
-		'gutenberg_figma_embed-cgb-style-css', // Handle.
+		'figma_embed_block-cgb-style-css', // Handle.
 		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
 		array( 'wp-editor' ), // Dependency to include the CSS after it.
 		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
@@ -38,7 +38,7 @@ function gutenberg_figma_embed_cgb_block_assets() { // phpcs:ignore
 
 	// Register block editor script for backend.
 	wp_register_script(
-		'gutenberg_figma_embed-cgb-block-js', // Handle.
+		'figma_embed_block-cgb-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
 		null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
@@ -47,7 +47,7 @@ function gutenberg_figma_embed_cgb_block_assets() { // phpcs:ignore
 
 	// Register block editor styles for backend.
 	wp_register_style(
-		'gutenberg_figma_embed-cgb-block-editor-css', // Handle.
+		'figma_embed_block-cgb-block-editor-css', // Handle.
 		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
 		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
 		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
@@ -64,16 +64,16 @@ function gutenberg_figma_embed_cgb_block_assets() { // phpcs:ignore
 	 * @since 1.16.0
 	 */
 	register_block_type(
-		'cgb/block-gutenberg-figma-embed', array(
+		'cgb/block-figma-embed-block', array(
 			// Enqueue blocks.style.build.css on both frontend & backend.
-			'style'         => 'gutenberg_figma_embed-cgb-style-css',
+			'style'         => 'figma_embed_block-cgb-style-css',
 			// Enqueue blocks.build.js in the editor only.
-			'editor_script' => 'gutenberg_figma_embed-cgb-block-js',
+			'editor_script' => 'figma_embed_block-cgb-block-js',
 			// Enqueue blocks.editor.build.css in the editor only.
-			'editor_style'  => 'gutenberg_figma_embed-cgb-block-editor-css',
+			'editor_style'  => 'figma_embed_block-cgb-block-editor-css',
 		)
 	);
 }
 
 // Hook: Block assets.
-add_action( 'init', 'gutenberg_figma_embed_cgb_block_assets' );
+add_action( 'init', 'figma_embed_block_cgb_block_assets' );
