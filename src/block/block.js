605e193f2efc8da955c21694c0812568060c820a
David Levin <sayhi@levinmedia.com>
2019-06-25 20:21:13 -0700

    update block title and maybe get the custom icon to work

--- src/block/block.js
+++ src/block/block.js
@@ -8,9 +8,7 @@
 //  Import CSS.
 import './style.scss';
 import './editor.scss';
-
-const { __ } = wp.i18n; // Import __() from wp.i18n
-const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+import icon from './icon.js';
 
 /**
  * Register: aa Gutenberg Block.
@@ -25,15 +23,16 @@ const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.b
  * @return {?WPBlock}          The block, if it has been successfully
  *                             registered; otherwise `undefined`.
  */
-registerBlockType( 'cgb/block-gutenberg-figma-embed', {
+registerBlockType( 'levinmedia/gutenberg-figma-embed', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'gutenberg-figma-embed - CGB Block' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	title: __( 'Figma' ), // Block title.
+	icon: icon.figma,
+	category: 'embed', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 	keywords: [
-		__( 'gutenberg-figma-embed — CGB Block' ),
-		__( 'CGB Example' ),
-		__( 'create-guten-block' ),
+		__( 'Figma' ),
+		__( 'Design' ),
+		__( 'Embed' ),
+		__( 'Image' ),
 	],
 
 	/**
@@ -49,17 +48,6 @@ registerBlockType( 'cgb/block-gutenberg-figma-embed', {
 		return (
 			<div className={ props.className }>
 				<p>— Hello from the backend.</p>
-				<p>
-					CGB BLOCK: <code>gutenberg-figma-embed</code> is a new Gutenberg block
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
 			</div>
 		);
 	},
@@ -76,17 +64,6 @@ registerBlockType( 'cgb/block-gutenberg-figma-embed', {
 		return (
 			<div>
 				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>gutenberg-figma-embed</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
 			</div>
 		);
 	},
