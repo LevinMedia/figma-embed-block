2ff633f0d732881bab6d961d8d97e16028f88f24
James Newell <james.newell@automattic.com>
2019-12-05 10:49:41 +1100

    different approach to styling

--- src/block/view/index.js
+++ src/block/view/index.js
@@ -4,9 +4,10 @@ export const View = ( props ) => {
 	const embedURL = `https://www.figma.com/embed?embed_host=share&url=${ encodeURIComponent( url ) }`;
 	return (
 		/* this extra wrapper div gets max-width set */
-		<div>
-			<div className={ `${ className } is-${ aspectRatio }` }>
+		<div className={ className }>
+			<div className={ `figma-embed-block figma-embed-block--${ aspectRatio }` }>
 				<iframe
+					className="figma-embed-block__embed"
 					key={ aspectRatio } /* force creation of a new iframe to force figma to update its layout */
 					src={ embedURL }
 					allowFullScreen
