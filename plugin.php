a70c894b6373de1fe51b51f3d19e8b28fa8b4f08
David Levin <sayhi@levinmedia.com>
2019-12-04 19:40:45 -0800

    update block name

--- plugin.php
+++ plugin.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: Gutenberg Figma Embed
- * Plugin URI: https://github.com/LevinMedia/gutenberg-figma-embed
+ * Plugin Name: Figma Embed Block
+ * Plugin URI: https://github.com/LevinMedia/figma-embed-block
  * Description: A Gutenberg block to embed Figma frames, pages or prototypes on your WordPress site.
  * Author: levinmedia & jameslnewell
- * Author URI: https://github.com/LevinMedia/gutenberg-figma-embed
+ * Author URI: https://github.com/LevinMedia/figma-embed-block
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
